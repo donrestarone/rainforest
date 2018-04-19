8f171809d14f978e199ff597c54a07f52e6a125e
donrestarone <shashikejayatunge@gmail.com>
2018-04-19 12:24:23 -0400

    index page works

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -1,2 +1,13 @@
 class ProductsController < ApplicationController
+	
+	def index 
+		@title = "All Products"
+		@products = Product.all
+	end 
+
+	def show 
+		@product = Product.find(params[:id])
+		@title = @product.name
+	end 
+
 end
