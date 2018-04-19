c3a84116db75e6a0a31e462d80c2582cfeb9c59a
donrestarone <shashikejayatunge@gmail.com>
2018-04-19 13:30:05 -0400

    test commit

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -5,9 +5,57 @@ class ProductsController < ApplicationController
 		@products = Product.all
 	end 
 
+	def create 
+		@title = 'Create New Product'
+
+		@product = Product.new
+		@product.name = params[:product][:name]
+		@product.description = params[:product][:description]
+		@product.price_in_cents = params[:product][:price_in_cents]
+
+		if @product.save
+			redirect_to products_path
+			#redirect_to product_path
+
+		else 
+			render :new
+		end 
+	end
+
+	def new
+		@title = 'Create new Product'
+		@product = Product.new
+	end 
+
+	#this is buggy
 	def show 
 		@product = Product.find(params[:id])
 		@title = @product.name
+	end 
+
+	def edit 
+		id = params[:id]
+		@product = Product.find(id)
+	end 
+
+	def update 
+		@product = Product.find(params[:id])
+
+		@product.name = params[:product][:name]
+		@product.description = params[:product][:description]
+		@product.price_in_cents = params[:product][:price_in_cents]
+
+		if @product.save
+			redirect_to product_path
+		else 
+			render :index
+		end
+	end 
+
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+		redirect_to products_path
 	end 
 
 end
--- rainforest/app/models/product.rb
+++ rainforest/app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
-	#has_and_belongs_to_many :customers
-	# has_many :customers #, through: :customer
-	has_many :reviews #, :through => :customers
+	has_many :reviews 
 	has_many :customers, through: :reviews
+
+	validates :name, presence: true, length: {in: 4..50}
+	validates :description, presence: true
+	validates :price_in_cents, numericality: {only_integer: true}
 
 end
